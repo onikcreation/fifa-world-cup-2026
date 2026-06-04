e4ef56934943ac7602a7b33378ab2d51387af742
Al Amin Onik <onikcreation@gmail.com>
2026-06-04 12:44:59 +0600

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBlp-_jw7ofV0yTUI4treAIDw9ZOF17Fik",
+  authDomain: "fifawc26onik.firebaseapp.com",
+  databaseURL: "https://fifawc26onik-default-rtdb.firebaseio.com",
+  projectId: "fifawc26onik",
+  storageBucket: "fifawc26onik.firebasestorage.app",
+  messagingSenderId: "766207347545",
+  appId: "1:766207347545:web:166cc979371d4c753a5a48",
+  measurementId: "G-Q6Y4ZKZVR9"
+};
+
+export const ADMIN_PASSWORD = "Onik@2026";
